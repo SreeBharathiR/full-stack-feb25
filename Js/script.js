178b3dd339fdf24d6f8e4e89c195d56482a74845
SreeBharathiR <bharathisree130@gmail.com>
2025-02-28 19:43:03 +0530

    JS Day 2

--- Js/script.js
+++ Js/script.js
@@ -1,9 +1,20 @@
-console.log(20 + 45);
+// console.log(20 + 45);
 let a = 10;
+let c = 50;
 
 {
   const dineshLocation = "erode";
 }
 a = 20;
+// console.log(a);
+// console.log(typeof dineshLocation);
+
+// console.log(typeof s);
+// console.log(typeof false);
+// console.log(typeof c);
+
+console.log(10 % 3);
+
+// a=a+1;
+console.log(++a);
 console.log(a);
-// console.log(dineshLocation);
