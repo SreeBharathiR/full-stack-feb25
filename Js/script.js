e34defcc9aefaf1382f1ae24c8f33c35dc17f7aa
SreeBharathiR <bharathisree130@gmail.com>
2025-03-03 19:48:19 +0530

    JS operators & conditional statement

--- Js/script.js
+++ Js/script.js
@@ -1,11 +1,11 @@
 // console.log(20 + 45);
-let a = 10;
-let c = 50;
+// let a = 10;
+// let c = 50;
 
-{
-  const dineshLocation = "erode";
-}
-a = 20;
+// {
+//   const dineshLocation = "erode";
+// }
+// a = 20;
 // console.log(a);
 // console.log(typeof dineshLocation);
 
@@ -13,8 +13,36 @@ a = 20;
 // console.log(typeof false);
 // console.log(typeof c);
 
-console.log(10 % 3);
+// console.log(10 % 3);
 
 // a=a+1;
-console.log(++a);
-console.log(a);
+// console.log(++a);
+// console.log(a);
+
+// Assignment operator
+
+let manoj = "107";
+let sathish = 106;
+let vignesh = 106;
+let total = manoj + manoj;
+
+// console.log(total);
+// manoj=manoj+3;
+// manoj += manoj;
+
+// console.log(manoj);
+
+console.log(sathish >= vignesh);
+
+console.log(1 === 1 || 1 < 0 || 1 == 3);
+
+console.log(!(1 > 1));
+
+if (manoj !== vignesh) {
+  // manoj += 5;
+  console.log("manoj and vignesh not roomates");
+} else if (sathish == vignesh) {
+  console.log("sathish & vignesh roomates");
+} else {
+  console.log("They are not a roomates");
+}
