890591e48ec7ef769f398ffe3333c75a27f21674
SreeBharathiR <bharathisree130@gmail.com>
2025-03-04 19:47:45 +0530

    Conditional statement & loop

--- Js/script.js
+++ Js/script.js
@@ -21,10 +21,10 @@
 
 // Assignment operator
 
-let manoj = "107";
-let sathish = 106;
-let vignesh = 106;
-let total = manoj + manoj;
+// let manoj = "107";
+// let sathish = 106;
+// let vignesh = 106;
+// let total = manoj + manoj;
 
 // console.log(total);
 // manoj=manoj+3;
@@ -32,17 +32,74 @@ let total = manoj + manoj;
 
 // console.log(manoj);
 
-console.log(sathish >= vignesh);
+// console.log(sathish >= vignesh);
 
-console.log(1 === 1 || 1 < 0 || 1 == 3);
+// console.log(1 === 1 || 1 < 0 || 1 == 3);
 
-console.log(!(1 > 1));
+// console.log(!(1 > 1));
 
-if (manoj !== vignesh) {
-  // manoj += 5;
-  console.log("manoj and vignesh not roomates");
-} else if (sathish == vignesh) {
-  console.log("sathish & vignesh roomates");
-} else {
-  console.log("They are not a roomates");
+// if (manoj !== vignesh) {
+//   // manoj += 5;
+//   console.log("manoj and vignesh not roomates");
+// } else if (sathish == vignesh) {
+//   console.log("sathish & vignesh roomates");
+// } else {
+//   console.log("They are not a roomates");
+// }
+
+//switch case
+
+// const movie = "leo";
+const movie = "dragon";
+const hero = "pradeep";
+const actress = "Ivana";
+
+// switch (movie) {
+//   case "leo":
+//     console.log("vijay");
+//     break;
+//   case "dragon":
+//     console.log("pradeep");
+//     break;
+//   default:
+//     console.log("Unable to identify the hero");
+//     break;
+// }
+
+// if (movie === "dragon") {
+//   console.log("Movie selected");
+//   if (hero === "pradeep") {
+//     console.log("Hero selected");
+//     if (actress === "anupamma") {
+//       console.log("Heroine selected");
+//       console.log("All are selected, ready to realse");
+//     } else {
+//       console.log("Movie is not ready");
+//     }
+//   }
+// }
+
+// ternary oprator
+
+// let a = 3;
+// let b = 5;
+// a > b ? console.log("A is greater") : console.log("B is greater");
+
+//loops
+
+// for (let i = 1; i < 100; i * 5) {
+//   console.log(`JavaScript ${i}`);
+// }
+
+let i = 1;
+while (i < 100) {
+  console.log(i);
+  // i = i - 3;
+  i = i * 5;
 }
+
+// tables
+
+// 1 X 2 = 2
+// 2 X 2 = 4
+// 3 X 2 = 6
