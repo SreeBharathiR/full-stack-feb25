e3c9d3da52936bec16ab2849fe68648e3fb96131
SreeBharathiR <bharathisree130@gmail.com>
2025-04-22 19:44:52 +0530

    DB connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,30 @@
 const express = require("express");
-
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
 const server = express();
 
-server.listen(3001, () => {
-  console.log("Server is running");
-});
+// mongoose
+//   .connect(process.env.MONGODB_URL)
+//   .then(() => {
+//     console.log("DB Connected!");
+//   })
+//   .catch((e) => {
+//     console.log(e.message);
+//   });
+
+const DBConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("DB connected!");
+    server.listen(process.env.PORT, () => {
+      console.log("Server is running");
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+DBConnection();
 
 server.get("/", (req, res) => {
   return res.send("Hello, What you want?");
